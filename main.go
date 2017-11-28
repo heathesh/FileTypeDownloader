bfabceae765af2586b706f00bfa0292ddf1b477b
Heathesh Bhandari <heatheshbhandari@Heatheshs-MacBook-Pro.local>
2017-11-28 19:09:10 +0200

    Added concurrency to downloads

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+  "sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -17,6 +18,7 @@ import (
 type XmlSettings struct {
 	XmlName            xml.Name
 	WebsiteUrl         string
+	BaseWebsiteUrl     string
 	FileTypeToDownload string
 	DownloadLocation   string
 }
@@ -26,15 +28,17 @@ func main() {
 
 	fmt.Println("Parsing HTML from", xmlSettings.WebsiteUrl)
 
-	linkScrape(xmlSettings.WebsiteUrl, xmlSettings.FileTypeToDownload, xmlSettings.DownloadLocation)
+	linkScrape(xmlSettings.WebsiteUrl, xmlSettings.BaseWebsiteUrl, xmlSettings.FileTypeToDownload, xmlSettings.DownloadLocation)
 }
 
-func linkScrape(websiteUrl string, fileType string, downloadLocation string) {
+func linkScrape(websiteUrl string, baseWebsiteUrl string, fileType string, downloadLocation string) {
 	doc, err := goquery.NewDocument(websiteUrl)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	var wg sync.WaitGroup
 
 	// use CSS selector found with the browser inspector
 	// for each, use index and item
@@ -44,7 +48,9 @@ func linkScrape(websiteUrl string, fileType string, downloadLocation string) {
 		linkHref := strings.TrimSpace(link)
 
 		if strings.HasSuffix(linkHref, fileType) {
-			if !strings.HasPrefix(linkHref, websiteUrl) {
+			if strings.HasPrefix(linkHref, "/") {
+				linkHref = baseWebsiteUrl + linkHref
+			} else if !strings.HasPrefix(linkHref, websiteUrl) {
 				linkHref = websiteUrl + "/" + linkHref
 			}
 
@@ -54,37 +60,50 @@ func linkScrape(websiteUrl string, fileType string, downloadLocation string) {
 			if _, err := os.Stat(fileName); os.IsNotExist(err) {
 				fmt.Println("Downloading", linkHref)
 
-				downloadFile(fileName, linkHref)
-
-				fmt.Println("Downloaded", fileName)
+        wg.Add(1)
+				go downloadFile(fileName, linkHref, &wg)
 			}
 		}
 	})
+
+  fmt.Println("Please wait, as each file completes you will be notified.")
+	wg.Wait()
+
+	fmt.Println("Done.")
 }
 
-func downloadFile(filepath string, url string) {
+func downloadFile(filepath string, url string, wg *sync.WaitGroup) {
 	// Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
+		fmt.Println("Failed download", filepath)
 		fmt.Println(err)
-		os.Exit(1)
+		wg.Done()
+		return
 	}
 	defer out.Close()
 
 	// Get the data
 	resp, err := http.Get(url)
 	if err != nil {
+		fmt.Println("Failed download", filepath)
 		fmt.Println(err)
-		os.Exit(1)
+		wg.Done()
+		return
 	}
 	defer resp.Body.Close()
 
 	// Writer the body to file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
+		fmt.Println("Failed download", filepath)
 		fmt.Println(err)
-		os.Exit(1)
+		wg.Done()
+		return
 	}
+
+  fmt.Println("Downloaded", filepath)
+	wg.Done()
 }
 
 func getSettings() XmlSettings {
